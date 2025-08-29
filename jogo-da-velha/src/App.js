1754327f0ac86183a37fc6815d83944f2536199c
VictorLemosbar <victor.escolas9045@gmail.com>
2025-08-28 22:35:54 -0300

    teste tabuleiro com square

---
+++ jogo-da-velha/src/App.js
@@ -0,0 +1,34 @@
+//Criação do elemento Square que irá compor o jogo da velha
+
+import './App.css';
+
+export default function Square() {
+  return(
+    <div>
+      <div>
+        <button className='square'>1</button>
+        <button className='square'>2</button>
+        <button className='square'>3</button>
+      </div>
+
+      <div>
+
+        <button className='square'>4</button>
+        <button className='square'>5</button>
+        <button className='square'>6</button>
+
+      </div>
+
+      <div>
+
+        <button className='square'>7</button>
+        <button className='square'>8</button>
+        <button className='square'>9</button>
+
+      </div>
+     
+   
+      
+    </div>
+  )
+}
