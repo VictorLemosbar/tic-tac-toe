4bfd65d0b7011beaea3198aa43896b145d1d4cac
VictorLemosbar <victor.escolas9045@gmail.com>
2025-09-04 19:30:35 -0300

    adicionando o userState para o X

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
+
 // Criação do elemento square que irá compor o tabuleiro do jogo da velha
 import './App.css';
 
 // Componente Square
-function Square({valor}){
+function Square({}){
+
+  const [valor, setValor] = useState(null)
 
   function handleClick() {
-    console.log("Botão Clicado")
+    setValor("X")
   }
 
   return(
@@ -17,21 +21,21 @@ export default function Tabuleiro(){
   return(
     <div>
       <div>
-        <Square valor = "1" />
-        <Square valor = "2" />
-        <Square valor = "3" />
+        <Square />
+        <Square  />
+        <Square  />
       </div>
 
       <div>
-        <Square valor = "4" />
-        <Square valor = "5" />
-        <Square valor = "6" />
+        <Square  />
+        <Square  />
+        <Square  />
       </div>
         
       <div>
-        <Square valor = "7" />
-        <Square valor = "8" />
-        <Square valor = "9" />
+        <Square  />
+        <Square  />
+        <Square  />
       </div>
     </div>
   );
